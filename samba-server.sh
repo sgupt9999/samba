0f66bdc8403fd206742bb2fed0aba410de1c16b9
Cloud User <ec2-user@ip-172-31-16-125.us-west-2.compute.internal>
2018-04-02 12:40:16 +0000

    New file. Set up samba server based on user inputs

---
+++ samba-server.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Create samba server and different configuration options
+SHAREDDIR="/myshare1"
+# User inputs
+
+
+# End of user inputs
+
+if [[ $EUID != "0" ]]
+then
+	echo "Error. You need to have root privileges to run this script"
+	exit 1
+fi
+
+PACKAGES="samba samba-common samba-client cifs-utils"
+# samba-client installs sambaclient and nmblookup
+# samba-common install the configuration file
+# samba installs server and client software
+# cifs utils installs utilities for mounting and managing CIFS mounts
+
+if yum list installed samba > /dev/null 2>&1
+then
+	echo "Removing pre-installed packages......."
+	systemctl -q is-active smb && {
+	systemctl -q stop smb
+	systemctl -q stop nmb
+	systemctl -q disable smb
+	systemctl -q disable nmb
+	}
+	yum remove -y -q -e0 $PACKAGES > /dev/null 2>&1
+fi
+
+echo "Installing packages......."
+yum install -y -q -e0 $PACKAGES
+echo "Done"
+
+systemctl start smb
+systemctl start nmb
+systemctl enable smb
+systemctl enable nmb
+
+if [ -f $SHAREDDIR ]
+then
+	echo "Directory already there"
+else
+	mkdir $SHAREDDIR
+fi
